4a8a6b0deb77bddefdc0dbeb40f1190b48e2e5fc
lluquereboloso <luis.luque.reboloso@alumnos.upm.es>
2017-09-29 14:10:39 +0200

    Creacion Proyecto

---
+++ example/src/main/java/vyv2017/grupo0/example/App.java
@@ -0,0 +1,13 @@
+package vyv2017.grupo0.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
